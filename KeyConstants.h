c5bb241a14da2c546f0e559db27129c08bf4807c
Amita Singh <amitasingh5661@gmail.com>
2019-01-03 10:41:43 +0530

    initial commit

---
+++ KeyConstants.h
@@ -0,0 +1,13 @@
+#ifndef KEYCONSTANTS_H
+#define KEYCONSTANTS_H
+
+#include <map>
+#include <string>
+
+class KeyPair
+{
+public:
+    KeyPair(const std:: string  &vk = "" const std::string &name = "")
+};
+
+#endif // KEYCONSTANTS_H
